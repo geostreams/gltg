5d2efa8cb6d52db685295cbb8381e60a1522ca51
Max Burnette <mburnet2@illinois.edu>
2023-11-14 13:15:48 -0600

    update labels for sidebar

--- src/containers/Summary/Sidebar.jsx
+++ src/containers/Summary/Sidebar.jsx
@@ -237,7 +237,7 @@ const Sidebar = ({ stationData, selectedNutrient,setSelectedNutrient,selectedTim
                         }}
                         input={<InputBase />}
                     >
-                        <option value="Nitrogen">Nitrogen</option>
+                        <option value="Nitrogen">Nitrate-N</option>
                     </NativeSelect>
                 </FormControl>
                 <FormControl
@@ -421,6 +421,12 @@ const Sidebar = ({ stationData, selectedNutrient,setSelectedNutrient,selectedTim
                                 title="Mean (dots) & Flow-Normalized (line) Concentration " />
                         </Box>}
                         </div>
+                        <a><b>Total Trend</b>: Flow-Normalized Non-Stationary Streamflow with 90% Confidence Interval</a>
+                        <br/><br/>
+                        <a><b>Source/Sink Component</b>: Flow-Normalized Stationary Streamflow with 90% Confidence Interval</a>
+                        <br/><br/>
+                        <a><b>Flow Component</b>: Total Trend - Source/Sink Component</a>
+                        <br/><br/>
                     </>)}
                 <br />
             </div>

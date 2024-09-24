b88a67eda85908a3e560cdb2405fc69bcf236701
vismayak <mohanar2@illinois.edu>
2024-09-24 15:29:52 -0500

    updated state portal resources

--- src/containers/StatePortal/sidebarConfig.js
+++ src/containers/StatePortal/sidebarConfig.js
@@ -7,18 +7,22 @@ export const sidebarConfig = {
 			{
 				name: "Arkansas Nutrient Reduction Strategy",
 				fileType: "pdf",
-				url: "https://google.com",
+				url: "https://www.agriculture.arkansas.gov/natural-resources/divisions/water-management/arkansas-nutrient-reduction-strategy/",
+			},
+			{
+				name: "Arkansas Nutrient Dashboard",
+				fileType: "dashboard",
+				url: "https://arkansas.greatlakestogulf.org/#/",
 			},
 			{
 				name: "Arkansas Nutrient Reduction Viewer",
 				fileType: "map",
-				url: "https://google.com",
+				url: "https://anrcdoc.aad.arkansas.gov/arcgisportal/apps/dashboards/92c5bf3078244aa6b1b2022236c5aae9",
 			},
-			{ name: "", fileType: "pdf", url: "https://google.com" },
 			{
-				name: "ANRS Story Map",
-				fileType: "map",
-				url: "https://google.com",
+				name: "Story Map",
+				fileType: "web",
+				url: "https://anrcdoc.aad.arkansas.gov/arcgisportal/apps/storymaps/stories/433a65c303e146df83eec5e5fe7ab9ad",
 			},
 		],
 	},
@@ -30,19 +34,50 @@ export const sidebarConfig = {
 			{
 				name: "Illinois Nutrient Loss Reduction Strategy",
 				fileType: "web",
-				url: "https://google.com",
+				url: "https://epa.illinois.gov/topics/water-quality/watershed-management/excess-nutrients/nutrient-loss-reduction-strategy.html",
 			},
 			{
 				name: "Illinois Nutrient Dashboard",
 				fileType: "dashboard",
-				url: "https://google.com",
+				url: "https://illinois.greatlakestogulf.org/",
 			},
 			{
 				name: "Water Quality Resources",
-				fileType: "map",
-				url: "https://google.com",
+				fileType: "web",
+				url: "https://extension.illinois.edu/nlr",
 			},
-			{ name: "Story Map", fileType: "map", url: "https://google.com" },
+			{
+				name: "Story Map",
+				fileType: "map",
+				url: "https://ilsustainableag.org/learn/conservation-story-map/?farmtype%5B%5D=demosite&farmtype%5B%5D=research&farmtype%5B%5D=service&farmtype%5B%5D=specialist&maplayers%5B%5D=Topographic",
+			},
+		],
+	},
+	indiana: {
+		header: "Indiana Nutrient Reduction Strategy",
+		subHeader:
+			"Lorem ipsum dolor sit amet consectetur. Netus adipiscing a enim arcu ullamcorper enim sed a dignissim.",
+		dataAndResources: [
+			{
+				name: "Indiana Nutrient Reduction Strategy",
+				fileType: "web",
+				url: "https://www.in.gov/isda/divisions/soil-conservation/indiana-state-nutrient-reduction-strategy/",
+			},
+			{
+				name: "Indiana Nutrient Dashboard",
+				fileType: "dashboard",
+				url: "https://gisdata.in.gov/portal/apps/experiencebuilder/experience/?id=a7a5365d78444f79bcddc1bbbf443d1a&page=Sites",
+			},
+			{
+				name: "Water Quality Resources",
+				fileType: "web",
+				url: "https://www.google.com",
+			},
+			{
+				name: "Story Map",
+				fileType: "web",
+				url: "https://storymaps.arcgis.com/stories/977fe35741c34a2b860b5702c797e020",
+			},
 		],
 	},
 	iowa: {
@@ -50,8 +85,241 @@ export const sidebarConfig = {
 		subHeader:
 			"Lorem ipsum dolor sit amet consectetur. Netus adipiscing a enim arcu ullamcorper enim sed a dignissim.",
 		dataAndResources: [
-			{ name: "Nutrient Loss Reduction Strategy", fileType: "web" },
-			{ name: "Iowa Nutrient Dashboard", fileType: "dashboard" },
+			{
+				name: "Nutrient Loss Reduction Strategy",
+				fileType: "web",
+				url: " https://www.nutrientstrategy.iastate.edu/",
+			},
+			{
+				name: "Iowa Nutrient Dashboard",
+				fileType: "dashboard",
+				url: " https://programs.iowadnr.gov/aquia/search/map",
+			},
+		],
+	},
+	kentucky: {
+		header: "Kentucky Nutrient Reduction Strategy",
+		subHeader:
+			"Lorem ipsum dolor sit amet consectetur. Netus adipiscing a enim arcu ullamcorper enim sed a dignissim.",
+		dataAndResources: [
+			{
+				name: "Kentucky Nutrient Reduction Strategy",
+				fileType: "web",
+				url: "https://eec.ky.gov/Environmental-Protection/Water/Protection/Pages/Nutrient-Reduction-Strategy.aspx",
+			},
+			{
+				name: "Kentucky Nutrient Dashboard",
+				fileType: "dashboard",
+				url: "https://kygis.maps.arcgis.com/apps/MapSeries/index.html?appid=401eaf6ea6e24ffa82985d122cf1bbb0",
+			},
+			{
+				name: "Water Quality Resources",
+				fileType: "web",
+				url: "https://eec.ky.gov/Natural-Resources/Conservation/Pages/AgWaterQualityPlan.aspx",
+			},
+			{
+				name: "Story Map",
+				fileType: "web",
+				url: "https://storymaps.arcgis.com/stories/ad5b60986f04440399964378c81b1da9",
+			},
+		],
+	},
+	louisiana: {
+		header: "Louisiana Nutrient Reduction Strategy",
+		subHeader:
+			"Lorem ipsum dolor sit amet consectetur. Netus adipiscing a enim arcu ullamcorper enim sed a dignissim.",
+		dataAndResources: [
+			{
+				name: "Louisiana Nutrient Reduction Strategy",
+				fileType: "web",
+				url: "https://www.deq.louisiana.gov/page/nutrient-management-strategy",
+			},
+			{
+				name: "Louisiana Nutrient Dashboard",
+				fileType: "dashboard",
+				url: "https://www.google.com",
+			},
+			{
+				name: "Water Quality Resources",
+				fileType: "web",
+				url: "https://waterdata.deq.louisiana.gov/",
+			},
+			{
+				name: "Story Map",
+				fileType: "web",
+				url: "https://www.google.com",
+			},
+		],
+	},
+	minnesota: {
+		header: "Minnesota Nutrient Reduction Strategy",
+		subHeader:
+			"Lorem ipsum dolor sit amet consectetur. Netus adipiscing a enim arcu ullamcorper enim sed a dignissim.",
+		dataAndResources: [
+			{
+				name: "Minnesota Nutrient Reduction Strategy",
+				fileType: "web",
+				url: "https://www.pca.state.mn.us/air-water-land-climate/reducing-nutrients-in-waters",
+			},
+			{
+				name: "Minnesota Nutrient Dashboard",
+				fileType: "dashboard",
+				url: "https://public.tableau.com/app/profile/mpca.data.services/viz/Long-termStreamTrends/Pollutantconcentrations",
+			},
+			{
+				name: "Water Quality Resources",
+				fileType: "dashboard",
+				url: "https://public.tableau.com/app/profile/mpca.data.services/viz/MinnesotaNutrientReductionStrategyBMPSummary/MinnesotaNutrientReductionStrategyBMPSummary",
+			},
+			//   { name: "Water Quality Resources", fileType: "web", url: "https://public.tableau.com/app/profile/mpca.data.services/viz/WatershedPollutantLoadReductionCalculator/WatershedPollutantLoadReductionCalculator" },
+			//   { name: "Water Quality Resources", fileType: "web", url: "https://public.tableau.com/app/profile/mpca.data.services/viz/CWAA-Bestmanagementpracticesbywatershed/Bestmangementpracticesbywatershed" },
+			//   { name: "Water Quality Resources", fileType: "web", url: "https://public.tableau.com/app/profile/mpca.data.services/viz/WatershedPollutantLoadMonitoringNetworkWPLMNDataViewer/ProgramOverview" },
+			{
+				name: "Story Map",
+				fileType: "web",
+				url: "https://www.google.com",
+			},
+		],
+	},
+	mississippi: {
+		header: "Mississippi Nutrient Reduction Strategy",
+		subHeader:
+			"Lorem ipsum dolor sit amet consectetur. Netus adipiscing a enim arcu ullamcorper enim sed a dignissim.",
+		dataAndResources: [
+			{
+				name: "Mississippi Nutrient Reduction Strategy",
+				fileType: "web",
+				url: "https://www.mdeq.ms.gov/water/surface-water/nonpoint-source-pollution-program/nutrient-reduction-in-mississippi/",
+			},
+			{
+				name: "Mississippi Nutrient Dashboard",
+				fileType: "dashboard",
+				url: "https://www.google.com",
+			},
+			{
+				name: "Water Quality Resources",
+				fileType: "web",
+				url: "https://www.google.com",
+			},
+			{
+				name: "Story Map",
+				fileType: "web",
+				url: "https://www.google.com",
+			},
+		],
+	},
+	missouri: {
+		header: "Missouri Nutrient Reduction Strategy",
+		subHeader:
+			"Lorem ipsum dolor sit amet consectetur. Netus adipiscing a enim arcu ullamcorper enim sed a dignissim.",
+		dataAndResources: [
+			{
+				name: "Missouri Nutrient Reduction Strategy",
+				fileType: "web",
+				url: "https://dnr.mo.gov/water/what-were-doing/water-planning/nutrient-loss-reduction-strategy",
+			},
+			{
+				name: "Missouri Nutrient Dashboard",
+				fileType: "dashboard",
+				url: "https://missouri.greatlakestogulf.org/#/",
+			},
+			{
+				name: "Water Quality Resources",
+				fileType: "web",
+				url: "https://dnr.mo.gov/water/hows-water/monitoring-data",
+			},
+			//   { name: "Water Quality Resources", fileType: "web", url: "https://nmtracker.missouri.edu/" },
+			{
+				name: "Story Map",
+				fileType: "web",
+				url: "https://www.google.com",
+			},
+		],
+	},
+	ohio: {
+		header: "Ohio Nutrient Reduction Strategy",
+		subHeader:
+			"Lorem ipsum dolor sit amet consectetur. Netus adipiscing a enim arcu ullamcorper enim sed a dignissim.",
+		dataAndResources: [
+			{
+				name: "Ohio Nutrient Reduction Strategy",
+				fileType: "web",
+				url: "https://epa.ohio.gov/divisions-and-offices/surface-water/reports-data/nutrient-pollution-finding-solutions",
+			},
+			{
+				name: "Ohio Nutrient Dashboard",
+				fileType: "dashboard",
+				url: "https://www.google.com",
+			},
+			{
+				name: "Water Quality Resources",
+				fileType: "web",
+				url: "https://www.google.com",
+			},
+			{
+				name: "Story Map",
+				fileType: "web",
+				url: "https://www.google.com",
+			},
+		],
+	},
+	tennessee: {
+		header: "Tennessee Nutrient Reduction Strategy",
+		subHeader:
+			"Lorem ipsum dolor sit amet consectetur. Netus adipiscing a enim arcu ullamcorper enim sed a dignissim.",
+		dataAndResources: [
+			{
+				name: "Tennessee Nutrient Reduction Strategy",
+				fileType: "web",
+				url: "https://www.tn.gov/environment/program-areas/wr-water-resources/nutrient-management-in-tennessee/partnerships/tennessee-nutrient-reduction-task-force.html",
+			},
+			{
+				name: "Tennessee Nutrient Dashboard",
+				fileType: "dashboard",
+				url: "https://tennessee.greatlakestogulf.org/#/",
+			},
+			{
+				name: "Water Quality Resources",
+				fileType: "web",
+				url: "https://www.tn.gov/environment/program-areas/wr-water-resources/tn-plant-optimization-programs/tnpop/case-studies.html",
+			},
+			{
+				name: "Story Map",
+				fileType: "web",
+				url: "https://www.google.com",
+			},
+		],
+	},
+	wisconsin: {
+		header: "Wisconsin Nutrient Reduction Strategy",
+		subHeader:
+			"Lorem ipsum dolor sit amet consectetur. Netus adipiscing a enim arcu ullamcorper enim sed a dignissim.",
+		dataAndResources: [
+			{
+				name: "Wisconsin Nutrient Reduction Strategy",
+				fileType: "web",
+				url: "https://dnr.wisconsin.gov/topic/SurfaceWater/NutrientStrategy.html",
+			},
+			{
+				name: "Wisconsin Nutrient Dashboard",
+				fileType: "dashboard",
+				url: "https://wisconsindnr.shinyapps.io/riverwq/",
+			},
+			{
+				name: "Water Quality Resources",
+				fileType: "web",
+				url: "https://rconnect.usgs.gov/glritrends/",
+			},
+			{
+				name: "Water Quality Resources",
+				fileType: "web",
+				url: "https://dnr.wisconsin.gov/topic/SurfaceWater/Monitoring.html",
+			},
+			{
+				name: "Story Map",
+				fileType: "web",
+				url: "https://www.google.com",
+			},
 		],
 	},
 };
